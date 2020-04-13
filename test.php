21fb8b95f1a20bc42ade875f831a33baff45cf26
stamander <iida.atsushi.fh@gmail.com>
2020-04-13 11:22:13 +0900

    Update test.php

--- test.php
+++ test.php
@@ -1,12 +1,8 @@
 <?php
-    // 'Hello, world!'をechoしてください
-    $s='Hello, world!';
-    echo $s
+  $name = "atsushi";
+  $name.= "iida";
+
+  echo $name;
   ?>
 
-  <?php
-    // 7 * 2をechoしてください
-    $sum = 7*2;
-    $sum1 = $sum*3;
-    echo $sum1;
-?>
+ 
