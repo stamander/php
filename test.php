733b07b40de79b5015d8a15c9ed9a6c3f610862b
stamander <iida.atsushi.fh@gmail.com>
2020-04-22 11:55:11 +0900

    Update test.php

--- test.php
+++ test.php
@@ -3,12 +3,13 @@
   if($x>30){
     echo '$xは30より大きい';
   }elseif($x>=20){
-    echo '$xは30以下20以上';
+    echo '$xは30以下20以上<br />';
 
   }else{
     echo '$xは20より小さい';
+    
   }
-  br;
+  
 
   $age = 23;
   if($age>=30){
