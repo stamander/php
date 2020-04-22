78940865d5d291b89f79f3d8102d3d6d1037b5d8
stamander <iida.atsushi.fh@gmail.com>
2020-04-22 12:27:58 +0900

    Update test.php

--- test.php
+++ test.php
@@ -3,15 +3,15 @@
   $a= $coin%3;
   
   switch($a){
-    case 0;
-      echo '表';
+    case 0:
+      echo '表':
     break;
 
-    case 1;
+    case 1:
       echo '裏';
     break;
 
-    default;
+    default:
       echo 'エラー';
     break;
   }
