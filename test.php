366b08d87ffc24ab5fe0967451602a083a7850ec
stamander <iida.atsushi.fh@gmail.com>
2020-04-22 12:38:15 +0900

    Update test.php

--- test.php
+++ test.php
@@ -4,7 +4,7 @@
   
   switch($a){
     case 0:
-      echo '表':
+      echo '表';
     break;
 
     case 1:
@@ -15,7 +15,7 @@
       echo 'エラー';
     break;
   }
-
+  br;
       // 変数$numを定義し、好きな数字を代入してください
       $num =11282920;
   
@@ -25,22 +25,26 @@
       // switch文を用いてください
       switch($remainder){
         case 0:
-        echo '大吉です。';
+          echo '大吉です。';
         break;
         
         case 1:
-        echo '中吉です。';
+          echo '中吉です。';
         break;
         
         case 2:
-        echo '小吉です。';
-        
+          echo '小吉です。';
         break;
         
         default:
-        echo '凶です。';
+          echo '凶です。';
         break;
       }
+
+  $names = array('ats','yukimi');
+  $names[] = 'stamander';
+  echo $names[1]
+  echo 
       
   ?>
 
