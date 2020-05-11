7475da06f9e37ec6d09889ae2ea378f24e736fc8
stamander <iida.atsushi.fh@gmail.com>
2020-05-11 12:17:14 +0900

    Update hero.rb

--- hero.rb
+++ hero.rb
@@ -9,12 +9,7 @@ p "月収は#{m}円です"
 
 
 
-characters = [
-  {name: "にんじゃわんこ", age: 14},
-  {name: "ひつじ仙人"},
-  {name: "ベイビーわんこ", age: 5},
-  {name: "とりずきん"}
-]
+
 
 # each文でそれぞれのキャラクターの名前を表示してください
   characters.each do |character|
