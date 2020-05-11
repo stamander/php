33fc7eea8d504d79a401892ed86da90f939a0a57
stamander <iida.atsushi.fh@gmail.com>
2020-05-11 12:07:36 +0900

    Update hero.rb

--- hero.rb
+++ hero.rb
@@ -20,3 +20,12 @@ users.each do |name|
 
 end
 
+exams = [
+  {subject: "Math", score: 80},
+  {subject: "Science", score: 55}
+]
+
+# each文を用いて、要素ごとに「○○の結果は△△点です」と出力してください
+exams.each do |exam|
+  puts "#{exam[:subject]}の結果は#{exam[:score]}点です"
+end
