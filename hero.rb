3dcadc33f9fc3cfa4485e78b3c87098edaa6de17
stamander <iida.atsushi.fh@gmail.com>
2020-05-11 11:53:35 +0900

    Update hero.rb

--- hero.rb
+++ hero.rb
@@ -33,8 +33,17 @@ else
   puts "#{user[:name]}さんの年齢は秘密です"
 
 
+end
+
+
+
+
+ exam = {subject: "Math"}
+
+ # 条件式にキー「:score」の値を使うif文をつくってください
+ if exam[:score]
+ puts "#{exam[:subject]}の結果は#{exam[:score]}点です"
+ else
+   puts "#{exam[:subject]}の結果はわかりません"
  end
-
-
-
-
+ 
