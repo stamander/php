6a1e4b9790b6f0098cae30ddaa22cee710064f06
stamander <iida.atsushi.fh@gmail.com>
2020-05-12 17:01:50 +0900

    Update hero.rb

--- hero.rb
+++ hero.rb
@@ -8,27 +8,3 @@ p "週給は#{s}円です"
 p "月収は#{m}円です"
 
 
-
-
-
-
-  characters = [
-    {name: "にんじゃわんこ", age: 14},
-    {name: "ひつじ仙人"},
-    {name: "ベイビーわんこ", age: 5},
-    {name: "とりずきん"}
-  ]
-  
-  characters.each do |character|
-    puts "--------------------"
-    puts "名前は#{character[:name]}です"
-    
-    # キー:ageの値に応じて年齢の情報を出力してください
-    if character[:age]
-      puts "年齢は#{character[:age]}歳です"
-    else
-      puts "年齢は秘密です"
-    end
-    
-  end
-  
