64da140de53369ce5431eff33365d151738b1cf6
stamander <iida.atsushi.fh@gmail.com>
2020-05-12 17:15:12 +0900

    Update hero.rb

--- hero.rb
+++ hero.rb
@@ -8,9 +8,10 @@ p "週給は#{s}円です"
 p "月収は#{m}円です"
 
 
-def introduce
+def introduce(name,age)
   puts "こんにちは"
-  puts "私はにんじゃわんこです"
+  puts "私は#{name}です"
+  puts "年齢は#{age}歳です"
 end
 
 
@@ -19,7 +20,10 @@ def print_info(name)
   puts "今日はヘッドホンがセール中です！"
 end
 
-introduce
+introduce("ニンジャわんこ",12)
+introduce("ミッキー",15)
+introduce("ミニー",18)
+
 # print_infoメソッドを呼び出してください
 print_info("ats")
 print_info("yuki")
