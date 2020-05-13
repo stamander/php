8532a62aca0e12330f3ead9047dfda6133958a52
stamander <iida.atsushi.fh@gmail.com>
2020-05-13 12:53:43 +0900

    Update hero.rb

--- hero.rb
+++ hero.rb
@@ -7,20 +7,31 @@ menu2 = Menu.new(name: "すし", price: 1000)
 menu3 = Menu.new(name: "コーラ", price: 300)
 menu4 = Menu.new(name: "お茶", price: 200)
 
+menus = [menu1, menu2, menu3, menu4]
+
 index = 0
-
-menus = [menu1,menu2,menu3,menu4]
-
 menus.each do |menu|
-  puts "#{index}.#{menu.info}"
+  puts "#{index}. #{menu.info}"
   index += 1
 end
 
+puts "--------------"
+puts "メニューの番号を選択してください"
 
-puts "名前を入力してね"
+# 入力を数値として受け取って変数orderに代入してください
+order = gets.chomp.to_i
 
-name = gets.chomp.to_i
+# 選択されたメニューのインスタンスを変数selected_menuに代入してください
+selected_menu = menus[order]
 
-puts "私の名前は#{name}です"
+# 「選択されたメニュー: ○○」となるように出力してください
+puts "選択されたメニュー: #{selected_menu.name}"
 
+puts "個数を入力してください(3つ以上で100円割引)"
 
+# 入力を数値として受け取って変数countに代入してください
+count = gets.chomp.to_i
+
+# 「お会計は○○円です」となるように出力してください
+
+puts "お会計は#{selected_menu.get_total_price(count)}円です"
