9d612f9f802f042b28702f3b7e4747eea4eaecb1
stamander <iida.atsushi.fh@gmail.com>
2020-05-13 13:17:54 +0900

    Update hero.rb

--- hero.rb
+++ hero.rb
@@ -27,10 +27,8 @@ selected_menu = menus[order]
 # 「選択されたメニュー: ○○」となるように出力してください
 puts "選択されたメニュー: #{selected_menu.name}"
 
-puts "個数を入力してください(3つ以上で100円割引)"
-
-# 入力を数値として受け取って変数countに代入してください
 count = gets.chomp.to_i
+
 
 # 「お会計は○○円です」となるように出力してください
 puts "お会計は#{selected_menu.get_total_price(count)}円です"
