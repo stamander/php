2e12a6e90326f52632af867cac71e8c30b9c9e52
stamander <iida.atsushi.fh@gmail.com>
2020-05-17 14:25:54 +0900

    Update hero.rb

---
+++ hero.rb
@@ -0,0 +1,52 @@
+def register_data
+
+  # 名前、パンチ力、キック力、ジャンプ力をユーザーに入力させ、保存する。
+  puts "名前を入力してください"
+  puts "パンチ力を入力してください(0 ~ 100)"
+  puts "キック力を入力してください(0 ~ 100)"
+  puts "ジャンプ力を入力してください(0 ~ 100)"
+
+end
+
+
+def power
+
+  # 入力された数字が0~100になるまで繰り返し入力させる。
+  puts "100以下の数字を入力してください"
+
+end
+
+
+def hero_rank(punch, kick, jump)
+
+  # ヒーロランクの判定をする。
+
+end
+
+
+def show_data_list(data)
+
+   puts "見たい番号を入力してください"
+  # 保存されたデータの一覧を出力する。
+
+  # 選択されたデータの詳細な情報（名前、パンチ力、キック力、ジャンプ力、ヒーロランク）を出力する。
+
+end
+
+while true
+  puts '選択してください'
+  puts '[0]登録する'
+  puts '[1]データを確認する'
+  puts '[2]終了する'
+  input = gets.to_i
+
+  if input == 0
+    # データの登録を行う。
+  elsif input == 1
+    # 保存されたデータの一覧を表示する。
+  elsif input == 2
+    exit
+  else
+    puts '無効な値です'
+  end
+end
