59e00b08d0e789195abc35c41e2fccee9180040d
stamander <iida.atsushi.fh@gmail.com>
2020-05-17 19:09:22 +0900

    Update Hercules.rb

--- Hercules.rb
+++ Hercules.rb
@@ -1,9 +1,9 @@
 require "./heros"
 puts "ヒーローの名前を入力してください"
 name = gets.chomp
-puts "パンチ力は？"
+puts "パンチ力は？(1~100までで入れてね)"
 punch = gets.chomp.to_i
-puts "キック力は？"
+puts "キック力は？(1~100までで入れてね)"
 kick = gets.chomp.to_i
 
 hero1 = Heros.new(name:name,punch:punch,kick:kick)
