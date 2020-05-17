cc0ccf627cf3db2313d73a7a083f6c938dc31be1
stamander <iida.atsushi.fh@gmail.com>
2020-05-17 15:01:47 +0900

    punch

--- Hercules.rb
+++ Hercules.rb
@@ -1,3 +1,7 @@
 puts "ヒーローの名前を入力してください"
 name = gets.chomp
-puts ""
+puts "あなたのパンチ力は？"
+punch = gets.chomp.to_i
+puts "あなたのキック力は？"
+kick = gets.chomp.to_i
+
---
+++ hero2.rb
@@ -0,0 +1,8 @@
+class hero2{
+  attr_accessor :name
+  attr_accessor :punch
+  attr_accessor :kick
+}
+
+def info
+  return "#{self.name}.#{self.punch}"
