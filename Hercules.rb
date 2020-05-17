b4f2a5d1884decd347d03458e14eb280eb9112e2
stamander <iida.atsushi.fh@gmail.com>
2020-05-17 17:25:14 +0900

    Update Hercules.rb

--- Hercules.rb
+++ Hercules.rb
@@ -1,8 +1,18 @@
-require "./heros"
+class Heros
+  attr_accessor :name
+  attr_accessor :punch
+  attr_accessor :kick
 
-class Hercules{
-  hero1 = Heros.new(name: "ats",punch:100,kick:50)
+  def initialized(name:,punch:,kick:)
+    puts "#{self.name},#{self.punch},#{self.kick}"
+  end
 
-}
+  def info
+    return "#{self.name}.#{self.punch}.#{self.kick}"
+  end
+end
+
+
+hero1 = Heros.new
 
 
