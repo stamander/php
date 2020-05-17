bba9153f274adfff3950aada87e65b5d761c5334
stamander <iida.atsushi.fh@gmail.com>
2020-05-17 14:48:14 +0900

    Update hero.rb

--- hero.rb
+++ hero.rb
@@ -1,31 +1,33 @@
-def register_data
+def register_data(data)
 
   # 名前、パンチ力、キック力、ジャンプ力をユーザーに入力させ、保存する。
   puts "名前を入力してください"
   name = gets.chomp
-
   puts "パンチ力を入力してください(0 ~ 100)"
-  punch = gets.chomp.to_i
-  power()
+  punch = power(gets.chomp.to_i)
 
   puts "キック力を入力してください(0 ~ 100)"
-  kick = gets.chomp.to_i
-  power()
-  puts "ジャンプ力を入力してください(0 ~ 100)"
-  jump = gets.chomp.to_i
-  power()
+  kick = power(gets.chomp.to_i)
 
-  
+  puts "ジャンプ力を入力してください(0 ~ 100)"
+  jump = power(gets.chomp.to_i)
+ 
+  person = {name:name,punch:punch,kick:kick}
+
+  data<<person
 
 end
 
 
-def power
-  if punch < 100 && kick < 100 && jump <100
+def power(power)
+  while power > 100
 
   # 入力された数字が0~100になるまで繰り返し入力させる。
     puts "100以下の数字を入力してください"
+    power = gets.chomp.to_i
   end
+
+  return power
 
 
 
@@ -40,13 +42,19 @@ end
 
 
 def show_data_list(data)
+  index = 0
 
-   puts "見たい番号を入力してください"
+  puts "見たい番号を入力してください"
   # 保存されたデータの一覧を出力する。
+  each.data do |person|
+    
+    puts "#{index}.#{person.name}"
 
   # 選択されたデータの詳細な情報（名前、パンチ力、キック力、ジャンプ力、ヒーロランク）を出力する。
 
 end
+
+data =[]
 
 while true
   puts '選択してください'
@@ -57,7 +65,7 @@ while true
 
   if input == 0
     # データの登録を行う。
-    register_data()
+    register_data(data)
   elsif input == 1
     show_data_list()
     # 保存されたデータの一覧を表示する。
