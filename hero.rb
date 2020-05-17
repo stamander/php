b18eacfc9f4e895398a3b7c230d1d41b397f9638
stamander <iida.atsushi.fh@gmail.com>
2020-05-17 14:51:37 +0900

    Update hero.rb

--- hero.rb
+++ hero.rb
@@ -1,60 +1,62 @@
 def register_data(data)
 
-  # 名前、パンチ力、キック力、ジャンプ力をユーザーに入力させ、保存する。
   puts "名前を入力してください"
   name = gets.chomp
   puts "パンチ力を入力してください(0 ~ 100)"
-  punch = power(gets.chomp.to_i)
-
+  punch = power(gets.to_i)
   puts "キック力を入力してください(0 ~ 100)"
-  kick = power(gets.chomp.to_i)
-
+  kick = power(gets.to_i)
   puts "ジャンプ力を入力してください(0 ~ 100)"
-  jump = power(gets.chomp.to_i)
- 
-  person = {name:name,punch:punch,kick:kick}
+  jump = power(gets.to_i)
 
-  data<<person
+  person = {name: name, punch: punch, kick: kick, jump: jump,  rank: hero_rank(punch, kick, jump)}
+  data << person
 
 end
 
-
 def power(power)
-  while power > 100
-
-  # 入力された数字が0~100になるまで繰り返し入力させる。
-    puts "100以下の数字を入力してください"
-    power = gets.chomp.to_i
-  end
-
-  return power
-
-
-
+    while power > 100
+      puts "100以下の数字を入力してください"
+        power = gets.to_i
+    end
+    return power
 end
 
 
 def hero_rank(punch, kick, jump)
 
-  # ヒーロランクの判定をする。
+  power = punch + kick + jump
+  if power == 300
+    rank = "A"
+  elsif 100 <= power && power < 300
+    rank = "B"
+  elsif 20 <= power && power < 100
+    rank = "C"
+  else
+    rank = "D"
+  end
 
 end
-
 
 def show_data_list(data)
-  index = 0
 
-  puts "見たい番号を入力してください"
-  # 保存されたデータの一覧を出力する。
-  each.data do |person|
-    
-    puts "#{index}.#{person.name}"
+  puts '見たい人の番号を選択してください'
+  data.each_with_index do |person, index|
+    puts "#{index + 1}: #{person[:name]}"
+  end
 
-  # 選択されたデータの詳細な情報（名前、パンチ力、キック力、ジャンプ力、ヒーロランク）を出力する。
+  input = gets.to_i - 1
+  person = data[input]
+  puts "名前: #{person[:name]}"
+  puts "パンチ力: #{person[:punch]}"
+  puts "キック力: #{person[:kick]}"
+  puts "ジャンプ力: #{person[:jump]}"
+  puts "ヒーローランク: #{person[:rank]}"
 
 end
 
-data =[]
+
+data = []
 
 while true
   puts '選択してください'
@@ -64,11 +66,9 @@ while true
   input = gets.to_i
 
   if input == 0
-    # データの登録を行う。
     register_data(data)
   elsif input == 1
-    show_data_list()
-    # 保存されたデータの一覧を表示する。
+    show_data_list(data)
   elsif input == 2
     exit
   else
